b8f97c06b30f5d71851ef9e5b134d2ee21bc909c
KavinSub <kavin.a.subramanyam@gmail.com>
2016-04-03 12:57:45 -0700

    Add port for Heroku dyno

--- flask_server.py
+++ flask_server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from infection import *
 
@@ -29,4 +30,5 @@ def graph(name):
 
 
 if __name__ == '__main__':
-	app.run()
+	port = int(os.environ.get("PORT", 31322))
+	app.run(host='0.0.0.0', port=port)
