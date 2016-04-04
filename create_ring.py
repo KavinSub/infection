cf1967aab2ea25cb338b9bf9ef7773be015770b3
KavinSub <kavin.a.subramanyam@gmail.com>
2016-04-03 20:40:26 -0700

    Change node ids for ring

--- create_ring.py
+++ create_ring.py
@@ -3,14 +3,14 @@
 # Write the nodes
 ring_users = open('ring_users.txt', 'w')
 for i in range(0, 30):
-	ring_users.write(str(i) + '\n')
+	ring_users.write('A' + str(i) + '\n')
 ring_users.close()
 
 # Write the edges
 ring_relations = open('ring_relations.txt', 'w')
 for i in range(0, 30):
-	node = i
-	adjacent_node = (i + 1) % 30
-	ring_relations.write(str(node) + " coaches " + str(adjacent_node) + '\n')
-	ring_relations.write(str(adjacent_node) + " is coached by " + str(node) + '\n')
+	node = 'A' + str(i) 
+	adjacent_node = 'A' + str((i + 1) % 30)
+	ring_relations.write(node + " coaches " + adjacent_node + '\n')
+	ring_relations.write(adjacent_node + " is coached by " + node + '\n')
 ring_relations.close()
